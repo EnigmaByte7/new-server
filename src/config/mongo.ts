151d2d9a43addb5fc644250ab9138f83d22af186
EnigmaByte7 <saxenay117@gmail.com>
2026-02-25 14:03:47 +0530

    readme

--- src/config/mongo.ts
+++ src/config/mongo.ts
@@ -1,6 +1,6 @@
 import mongoose from "mongoose";
 
 export async function initMongo() {
-    await mongoose.connect('mongodb+srv://saxenay117_db_user:nsmAe0GOf7nBemTc@cluster0.tlyjkaz.mongodb.net/?appName=Cluster0')
+    await mongoose.connect(process.env.MONGODB)
     console.log('mongo connected')
 }
